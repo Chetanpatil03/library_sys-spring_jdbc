746f6647c9bf1a791052623043f224e4e1c3803a
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-01-02 02:15:08 +0530

    created the row mapper for bean class Book

--- src/main/java/com/cb/mapper/BookRowMapper.java
+++ src/main/java/com/cb/mapper/BookRowMapper.java
@@ -1,6 +1,26 @@
 package com.cb.mapper;
 
-public class BookRowMapper implements 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.cb.bean.Book;
+
+public class BookRowMapper implements RowMapper<Book>
 {
 
+	@Override
+	public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Book book = new Book();
+		
+		book.setId(rs.getInt("book_id"));
+		book.setTitle(rs.getString("title"));
+		book.setAuthor(rs.getString("author"));
+		book.setYear(rs.getInt("year"));
+		book.setQty(rs.getInt("quantity"));
+		
+		return book;
+	}
+	
 }
