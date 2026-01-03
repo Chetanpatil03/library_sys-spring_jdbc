386c31093e9af488badd981b4f53bc9b93e5ebcd
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-01-03 14:27:12 +0530

    Create UserService.java

---
+++ src/main/java/com/cb/service/UserService.java
@@ -0,0 +1,19 @@
+package com.cb.service;
+
+public class UserService {
+	
+}
+
+/*
+ * - Register new user
+ * - Update user details
+ * - Remove user
+ * - View all users
+ * Find user by ID
+ * Find user by name
+ * Check if user exists
+ * Check if user is allowed to borrow books
+ * Get number of books currently borrowed by user
+ * Activate user
+ * Deactivate user
+ */
