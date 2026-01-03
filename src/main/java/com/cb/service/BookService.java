7a5569cf7f6a479c56036d7230f2b42c4d822497
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-01-03 14:27:11 +0530

    Create BookService.java

---
+++ src/main/java/com/cb/service/BookService.java
@@ -0,0 +1,53 @@
+package com.cb.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class BookService {
+	
+	public void addBook() {
+		
+	}
+	
+	public void updateBook() {
+		
+	}
+	
+	public void removeBook() {
+		
+	}
+	
+	public void viewAllBooks() {
+		
+	}
+	
+	public void viewAvailableBooks() {
+		
+	}
+	
+	public void sdd() {
+		
+	}
+
+	@Transactional
+	public void issueBook() {
+
+	}
+
+//	 ==> Methods 
+	/*
+	 * Add a new book to library
+	 * Update book details
+	 * Remove a book from library
+	 * View all books
+	 * View available books (quantity > 0)
+	 * Find book by ID
+	 * Find books by title
+	 * Find books by author
+	 * Increase book quantity
+	 * Decrease book quantity
+	 * Check book availability
+	 * Check if book exists
+	 */
+}
