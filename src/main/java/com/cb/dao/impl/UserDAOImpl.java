26053278a2c39698724cb418ba7207c012268214
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-01-03 16:47:36 +0530

    Create UserDAOImpl.java

---
+++ src/main/java/com/cb/dao/impl/UserDAOImpl.java
@@ -0,0 +1,64 @@
+package com.cb.dao.impl;
+
+import java.util.List;
+
+import com.cb.bean.User;
+import com.cb.dao.UserDAO;
+
+public class UserDAOImpl implements UserDAO {
+
+	@Override
+	public void addUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteUser(int user_id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public User getUser(int user_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isUserExist() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<User> getAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getUsersbyName(int user_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getActiveUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getDeactiveUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
