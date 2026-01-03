7fc744c3c4bdb4cc7f70f5d3acc4dcc1ec5d51df
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-01-03 16:47:29 +0530

    Create BookDAOImpl.java

---
+++ src/main/java/com/cb/dao/impl/BookDAOImpl.java
@@ -0,0 +1,76 @@
+package com.cb.dao.impl;
+
+import java.util.List;
+
+import com.cb.bean.Book;
+import com.cb.dao.BookDAO;
+
+public class BookDAOImpl implements BookDAO {
+
+	@Override
+	public void addBook(Book book) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateBook(Book book) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteBook(int book_id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Book getBook(int book_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Book> getAllBooks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Book> getBookByTitle(String title) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Book> getBookByAuthor(String author) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Book> getAvailableBooks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateQuantity(int qty) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void currentQuantity(int book_id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isBooKExist(int book_id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
